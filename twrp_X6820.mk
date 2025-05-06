9c19138ff0489b979fa07e02edd7793aeb3e3d62
hunter2986767 <156557124+hunter2986767@users.noreply.github.com>
2025-05-07 00:20:50 +0800

    Update twrp_X6820.mk

--- twrp_X6820.mk
+++ twrp_X6820.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit from fleur device
+# Inherit from X6820 device
 $(call inherit-product, device/infinix/X6820/device.mk)
 
 # Inherit some common TWRP stuff.
