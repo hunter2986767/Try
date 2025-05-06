dca79caaaf9e9b2d734cb3bbe1c4f961590c9e05
hunter2986767 <156557124+hunter2986767@users.noreply.github.com>
2025-05-06 23:53:35 +0800

    Update twrp_X6820.mk

--- twrp_X6820.mk
+++ twrp_X6820.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from fleur device
-$(call inherit-product, device/xiaomi/pissarro/device.mk)
+$(call inherit-product, device/infinix/X6820/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
