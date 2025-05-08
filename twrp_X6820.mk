96ad38605cd5f21f0cc48baca77de23c44e821fe
hunter2986767 <156557124+hunter2986767@users.noreply.github.com>
2025-05-08 22:36:22 +0800

    Update twrp_X6820.mk

--- twrp_X6820.mk
+++ twrp_X6820.mk
@@ -4,17 +4,23 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit from X6820 device
 $(call inherit-product, device/infinix/X6820/device.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
-# Product Specifics
-PRODUCT_NAME := X6820
-PRODUCT_DEVICE := twrp_X6820
-PRODUCT_BRAND := infinix
-PRODUCT_MODEL := X6820
-PRODUCT_MANUFACTURER := infinix 
+PRODUCT_DEVICE := X6820
+PRODUCT_NAME := twrp_X6820
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := INFINIX ZERO ULTRA 
+PRODUCT_MANUFACTURER := infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
